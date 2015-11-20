c863ceb651dd7a9268e2e43206476f4d07fe8b44
gabriel <gabriel@Psycho-Note>
2015-11-20 17:22:33 -0200

    Adicionado exemplo de variável

---
+++ src/com/pedrogf89/tcc/cap4/IdentificadoresValidos.java
@@ -0,0 +1,11 @@
+package com.pedrogf89.tcc.cap4;
+
+public class IdentificadoresValidos {
+	int __654;
+	String $aB123;
+	float floatDouble;
+	double NUMEROS;
+	char Texto1;
+	
+
+}
---
+++ src/com/pedrogf89/tcc/variavel/Variavel.java
@@ -0,0 +1,17 @@
+package com.pedrogf89.tcc.variavel;
+
+import java.lang.*;
+
+public class Variavel {
+	static String teste;
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 5; i++)
+			System.out.print(i + " ");
+
+		int i = 4;
+		System.out.println(i);
+
+	}
+}
