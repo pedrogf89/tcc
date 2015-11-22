8b2eadbb24c7111a8426b162ef558fe2b05a9d35
gabriel <gabriel@Psycho-Note>
2015-11-22 16:00:24 -0200

    Alterado formatação do arquivo.

--- src/com/pedrogf89/tcc/variavel/VariavelStatica.java
+++ src/com/pedrogf89/tcc/variavel/VariavelStatica.java
@@ -3,8 +3,6 @@ package com.pedrogf89.tcc.variavel;
 public class VariavelStatica {
 	static int var = 10;
 
-
-
 	public static void main(String[] args) {
 		VariavelStatica vS = new VariavelStatica();
 		System.out.println(vS.var);
