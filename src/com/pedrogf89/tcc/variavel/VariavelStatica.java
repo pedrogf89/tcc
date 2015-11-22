9c52679940335be675e637cd7eef0dd193b0fa5c
gabriel <gabriel@Psycho-Note>
2015-11-22 16:18:34 -0200

    Incluido variavel de instancia.

--- src/com/pedrogf89/tcc/variavel/VariavelStatica.java
+++ src/com/pedrogf89/tcc/variavel/VariavelStatica.java
@@ -2,9 +2,16 @@ package com.pedrogf89.tcc.variavel;
 
 public class VariavelStatica {
 	static int var = 10;
+	String nome;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 	public static void main(String[] args) {
+
 		VariavelStatica vS = new VariavelStatica();
+		vS.setNome("Pedro");
 		System.out.println(vS.var);
 		System.out.println(VariavelStatica.var);
 	}
